ec56c7754b08ef5a624abbcb9615ba140feea04c
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-04-23 20:54:19 -0700

    added classList to unread notifs

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const readAllMessagesAnchor = document.querySelector('.readLink');
 
 const readMessages = () => {
     const notifs = document.querySelectorAll('.notifications');
-    for (let i = 0; i < notifs.length; i++) {
-        if (notifs[i].classList.contains('unread')) {
-            // loggging to see if the if statement logs correct index and text
-            console.log(notifs[i], i, notifs[i].innerText);
-        }
-    }
+    // for (let i = 0; i < notifs.length; i++) {
+        notifs.forEach(notif => {
+            if (notif.classList.contains('unread')) {
+                notif.style.backgroundColor = 'white';
+            }
+        });
+    //     if (notifs.classList.contains('unread')) {
+    //         notifs
+    //     }
+    // }
+    // return notifs
 }
 readMessages();
