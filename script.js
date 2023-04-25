d84dd8a3daf44979f9e487494df5d9c7dc4f596c
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-04-24 20:21:07 -0700

    refactored function to utilized one loop

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ const redDotNotif = document.querySelectorAll('.dot');
 const readMessages = () => {
     const notifs = document.querySelectorAll('.notifications');
     readAllMessagesAnchor.addEventListener('click', () => {
-        notifs.forEach(notif => {
-            if (notif.classList.contains('unread')) {
-                notif.style.backgroundColor = 'white';
-            }
-        redDotNotif.forEach(dot => {
-            dot.style.display = 'none';
-        })
+        // notifs.forEach(notif => {
+        //     if (notif.classList.contains('unread')) {
+        //         notif.style.backgroundColor = 'white';
+        //     }
+        // redDotNotif.forEach(dot => {
+        //     dot.style.display = 'none';
+        // })
           
-        });
+        // });
+
+        for (let i = 0; i < notifs.length; i++) {
+            notifs[i].style.backgroundColor = 'white';
+            redDotNotif[i].style.display = 'none';
+        }
     })
        
-  
+
 }
 
 readMessages();
