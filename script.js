15e0d281cfb989d3dda8618d51402bebebaa3b70
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-04-24 20:29:09 -0700

    delete commented out code and added bg hover color on message

--- script.js
+++ script.js
@@ -4,19 +4,9 @@ const redDotNotif = document.querySelectorAll('.dot');
 const readMessages = () => {
     const notifs = document.querySelectorAll('.notifications');
     readAllMessagesAnchor.addEventListener('click', () => {
-        // notifs.forEach(notif => {
-        //     if (notif.classList.contains('unread')) {
-        //         notif.style.backgroundColor = 'white';
-        //     }
-        // redDotNotif.forEach(dot => {
-        //     dot.style.display = 'none';
-        // })
-          
-        // });
-
         for (let i = 0; i < notifs.length; i++) {
             notifs[i].style.backgroundColor = 'white';
-            redDotNotif[i].style.display = 'none';
+            redDotNotif[i].style.display = 'none'
         }
     })
        
