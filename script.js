3a3fe7bf6411bc5b00c45a945d85be49ea44336e
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-04-25 20:49:29 -0700

    created function to reset notifs to zero

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const readAllMessagesAnchor = document.querySelector('.readLink');
-const redDotNotif = document.querySelectorAll('.dot');
+const numberNotification = () => {
+    let numberNotif = document.getElementById('notif');
+    numberNotif.innerText = 0;
+    
+};
 
 const readMessages = () => {
-    const notifs = document.querySelectorAll('.notifications');
-    readAllMessagesAnchor.addEventListener('click', () => {
-        for (let i = 0; i < notifs.length; i++) {
-            notifs[i].style.backgroundColor = 'white';
-            redDotNotif[i].style.display = 'none'
-        }
-    })
-       
+  const notifs = document.querySelectorAll(".notifications");
+  const readAllMessagesAnchor = document.querySelector(".readLink");
+  const redDotNotif = document.querySelectorAll(".dot");
 
-}
+  readAllMessagesAnchor.addEventListener("click", () => {
+    numberNotification();
+    for (let i = 0; i < notifs.length; i++) {
+      notifs[i].style.backgroundColor = "white";
+      redDotNotif[i].style.display = "none";
+    }
+  
+  });
+ 
+};
+
 
 readMessages();
+
